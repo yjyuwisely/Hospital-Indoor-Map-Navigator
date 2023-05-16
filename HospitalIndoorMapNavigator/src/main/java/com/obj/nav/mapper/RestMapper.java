7851e364b151aa81784e0d60cfc9d292de73faf0
yjyuwisely <yjyuwisely@gmail.com>
2023-05-16 15:35:53 +0900

    Added Spring MVC.

---
+++ HospitalIndoorMapNavigator/src/main/java/com/obj/nav/mapper/RestMapper.java
@@ -0,0 +1,8 @@
+package com.obj.nav.mapper;
+
+import com.obj.nav.model.RestModel;
+
+public interface RestMapper {
+	
+	public RestModel location(String location);
+}
---
+++ HospitalIndoorMapNavigator/src/main/java/com/obj/nav/service/RestService.java
@@ -0,0 +1,8 @@
+package com.obj.nav.service;
+
+import com.obj.nav.model.RestModel;
+
+public interface RestService {
+	
+	public RestModel location(String location);
+}
